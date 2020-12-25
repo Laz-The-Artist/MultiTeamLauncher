c67a3aba6a749f576d69f6dff78a8ec3cf25bea9
King of Creepers-MultiTeam <33374497+King-of-Creepers@users.noreply.github.com>
2020-12-25 02:45:44 +0100

    app icon and more css

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -46,11 +46,11 @@ function loadGameTab() {
         {
             name: "JASG",
             version: {
-                name: "WorldGenTest",
+                name: "WorldGenTest - TechDemo 1",
                 versionNumber: "0.0.0"
             },
             iconURL: "./res/img/game_icon_jasg.png",
-            previewURL: "https://www.americasfinestlabels.com/images/CCS400PL.jpg",
+            previewURL: "./res/img/preview_jasg.png",
             descriptionTitle: "Just a survival game",
             description: "JASG is a fantasy 8-bit Survival game, within a topdown-perspective world.<br><br>This amazing new experience comes with a lot of exploration, advancing through this mysterious world, achieving magic, defeating bosses, crafting equipment and many more! Have i mentioned that its like 70% procedural?",
             downloadURL: "",
@@ -63,7 +63,7 @@ function loadGameTab() {
                 versionNumber: "0.0.0"
             },
             iconURL: "./res/img/game_icon_soto.png",
-            previewURL: "https://www.americasfinestlabels.com/images/CCS400PL.jpg",
+            previewURL: "./res/img/placeholder_preview.png",
             descriptionTitle: "Description",
             description: "Soldiers of the office is a story based, 2D platformer-shooter, all about the weird war of humans against an infection that spreads with ultra-modern technology.",
             downloadURL: "",
@@ -76,7 +76,7 @@ function loadGameTab() {
                 versionNumber: "0.0.0"
             },
             iconURL: "./res/img/game_icon_pf.png",
-            previewURL: "https://www.americasfinestlabels.com/images/CCS400PL.jpg",
+            previewURL: "./res/img/placeholder_preview.png",
             descriptionTitle: "Description",
             description: "Project Freedom is a 3D game, but its not what you would expect.<br><br>This game will have a play-mode for every type of game, like minecraft, pubg, cs:go, lol, overwatch, and many more new custom or weird play-modes",
             downloadURL: "",
