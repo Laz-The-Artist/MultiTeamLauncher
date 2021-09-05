9e103f676cf498d5d3bf78aa2f5449846944b9ac
Cat Core <arthur.koalas@free.fr>
2021-09-05 20:29:40 +0200

    fix update arrow

--- multiteamlauncher/src/components/games/nav/GameNav.vue
+++ multiteamlauncher/src/components/games/nav/GameNav.vue
@@ -1,17 +1,12 @@
 <template>
     <div id="game-list" class="sidebar">
-        <!-- <GameIcon v-for="game in gameList"
-        v-bind:bgColor="game.bgColor"
-        v-bind:iconPath="game.iconPath"
-        v-bind:key="game.index"
-        ></GameIcon> -->
         <div class="sidebar_item" 
         v-for="game in gameList"
         v-bind:key="game.index"
         v-bind:style="{ backgroundColor: game.color}"
         >
             <img v-bind:src="game.iconURL" alt="sidebar_item_icon" class="sidebar_item_icon" draggable="false">
-        <i class="fas fa-arrow-alt-circle-down" id="sidebar_item_badge" style="color=#4CBE6A"></i>
+            <i class="fas fa-arrow-alt-circle-down" id="sidebar_item_badge" v-bind:style="{color: '#4CBE6A'}"></i>
         </div>
     </div>
 </template>
