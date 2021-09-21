27f2153fa5ae81c05f5cfa1584759891d1b9db64
Cat Core <arthur.koalas@free.fr>
2021-09-21 15:57:37 +0200

    build

--- multiteamlauncher/vue.config.js
+++ multiteamlauncher/vue.config.js
@@ -1,13 +1,49 @@
 module.exports = {
     configureWebpack: (config) => {
         config.module.rules.push({
-        resourceQuery: /blockType=fluent/,
-        loader: 'fluent-vue-loader'
+            resourceQuery: /blockType=fluent/,
+            loader: 'fluent-vue-loader'
         })
 
         config.module.rules.push({
-        test: /\.ftl$/,
-        loader: 'raw-loader'
+            test: /\.ftl$/,
+            loader: 'raw-loader'
         })
+    },
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                // options placed here will be merged with default configuration and passed to electron-builder
+                linux: {
+                    target: [
+                        {
+                            target: "AppImage",
+                            arch: "x64"
+                        },
+                        {
+                            target: "snap",
+                            arch: "x64"
+                        },
+                        {
+                            target: "deb",
+                            arch: "x64"
+                        },
+                        {
+                            target: "rpm",
+                            arch: "x64"
+                        },
+                        {
+                            target: "pacman",
+                            arch: "x64"
+                        },
+                        {
+                            target: "tar.xz",
+                            arch: "x64"
+                        }
+                    ],
+                    category: "Game"
+                }
+            }
+        }
     }
 }
